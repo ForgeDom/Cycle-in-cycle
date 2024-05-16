a2ca50a2e0b7dab7288364165468c5cef5e3e4df
ForgeDom <maxstill2006@gmail.com>
2024-05-16 18:56:57 +0300

    Lab ex1 done

--- main.cpp
+++ main.cpp
@@ -6,5 +6,25 @@ int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	int a, b, c;
+	cout << "¬вед≥ть першу межу:" << endl;
+	cin >> a;
+	cout << "¬вед≥ть другу межу:" << endl;
+	cin >> b;
+	while (a >= b) {
+		cout << "ѕерша межа маЇ бути менша" << endl;
+		cout << "¬вед≥ть першу межу:" << endl;
+		cin >> a;
+		cout << "¬вед≥ть другу межу:" << endl;
+		cin >> b;
+	}
+	do {
+		cout << "¬вед≥ть число:" << endl;
+		cin >> c;
+		if (c < a || c > b) {
+			cout << "„исло не входить в д≥апазон" << endl;
+		}
+	} while(c < a || c > b);
+	cout << "„исло входить в д≥апазон!" << endl;
 	return 0;
 }
